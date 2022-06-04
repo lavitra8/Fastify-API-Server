7b5aa0aab05364a4b0c219dc808848960ac81e2e
lavitra8 <59783162+lavitra8@users.noreply.github.com>
2022-06-04 19:03:50 +0530

    Added new list files

---
+++ classes.js
@@ -0,0 +1,22 @@
+let classes = [
+  {
+    class_id: "1",
+    class_name: "Physics",
+    class_teacher_id: "",
+    student_list: [],
+  },
+  {
+    class_id: "2",
+    class_name: "Chemistry",
+    class_teacher_id: "",
+    student_list: [],
+  },
+  {
+    class_id: "3",
+    class_name: "Maths",
+    class_teacher_id: "",
+    student_list: [],
+  },
+];
+
+module.exports = classes;
---
+++ principal.js
@@ -0,0 +1,12 @@
+let principal = [
+  {
+    teacher_id: "1",
+    email: "patil@pccoe.com",
+    password: "password",
+    name: "Sonali Patil",
+    gender: "female",
+    meta_data: "",
+  },
+];
+
+module.exports = principal;
