e41cb70f7a24b103d4d46d62d4a0fae2bf2d4dae
SahilHemnani777 <hemnanisahil777@gmail.com>
2022-06-05 12:26:15 +0530

    setup for my self

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const PORT = 3000
 
 const students = require('./students')
 
+
+/// Sahil- Is there a need of this api end point ?
 // Declare a route
 fastify.get("/students", (request, reply) => {
   reply.send(students);
 });
 
+
+/// Sahil- Is there a need of this api end point ?
 //Param ID in request
 fastify.get("/students/:id", (request, reply) => {
 
