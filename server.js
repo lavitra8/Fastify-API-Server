9316670b0a43c385f68b06c77fb725728fff0726
lavitra8 <59783162+lavitra8@users.noreply.github.com>
2022-06-07 11:20:04 +0530

    Added JWT hook

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
 
+
 fastify.register(require('fastify-swagger'), {
   exposeRoute: true,
   routePrefix: '/docs',
   swagger: {
     info: {title:'fastify-api' },
   }
+})
+
+fastify.register(require("@fastify/jwt"), {
+  secret: "!@#$%^&*()SECRET)(*&^%$#@!",
+});
+
+fastify.addHook("onRequest", async (request, reply) => {
+  try {
+    await request.jwtVerify();
+  } catch (err) {
+    reply.send(err);
+  }
+});
+
+fastify.post('/signup', (req, reply) => {
+  const { email, password } = req.body;
+  let actor = "student";
+  const token = fastify.jwt.sign({ email, actor }, { expiredIn: 86400 });
+  reply.send({ token });
 })
 
 fastify.register(require('./routes/students'))
