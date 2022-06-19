e8b260d7daf45336a4a11b7d66118aa6038503aa
SahilHemnani777 <hemnanisahil777@gmail.com>
2022-06-19 16:32:48 +0530

    added keyCloak support

--- server.js
+++ server.js
@@ -1,88 +1,8 @@
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
-fastify.register(require('fastify-jwt'), {
-  secret: '##sEcReTkEy##' // should not be done like this (there should be an env variable)
-});
 
 
-fastify.register(require('fastify-swagger'), {
-  exposeRoute: true,
-  routePrefix: '/docs',
-  swagger: {
-    info: { title: 'fastify-api' },
-  }
-})
-
-let students = require("./students");
-fastify.post("/loginStudent", (req, res) => {
-  try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      res.status(400).send({ error: true, msg: 'Mandatory params missing.' });
-      return;
-    }
-    const student = students.find((students) => students.password == password && students.email == email);
-    if (!student) {
-      res.send({ 'login unsucessfull': 'invalid email or password' });
-    } else {
-      let actor = 'student';
-      const token = fastify.jwt.sign({ email, actor }, { expiresIn: 86400 });
-      res.status(200).send({ token, email });
-    }
-  } catch (error) {
-    res.send(error);
-  }
-})
-
-
-let teachers = require("./teachers");
-fastify.post("/loginTeacher", (req, res) => {
-  try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      res.status(400).send({ error: true, msg: 'Mandatory params missing.' });
-      return;
-    }
-    const student = teachers.find((teachers) => teachers.password == password && teachers.email == email);
-    if (!student) {
-      res.send({ 'login unsucessfull': 'invalid email or password' });
-    } else {
-      let actor = 'teacher';
-      const token = fastify.jwt.sign({ email, actor }, { expiresIn: 86400 });
-      res.status(200).send({ token, email });
-    }
-  } catch (error) {
-    res.send(error);
-  }
-})
-
-let principal = require("./principal");
-fastify.post("/loginPrincipal", (req, res) => {
-  try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      res.status(400).send({ error: true, msg: 'Mandatory params missing.' });
-      return;
-    }
-    const student = principal.find((principal) => principal.password == password && principal.email == email);
-    if (!student) {
-      res.send({ 'login unsucessfull': 'invalid email or password' });
-    } else {
-      let actor = 'principal';
-      const token = fastify.jwt.sign({ email, actor }, { expiresIn: 86400 });
-      res.status(200).send({ token, email });
-    }
-  } catch (error) {
-    res.send(error);
-  }
-})
-
-
-fastify.register(require('./routes/students'))
-fastify.register(require("./routes/teachers"));
-fastify.register(require("./routes/classes"));
-
-const PORT = 3000
+const PORT = 1234
 
 const start = async () => {
   try {
@@ -93,4 +13,12 @@ const start = async () => {
   }
 };
 
+fastify.post("/auth", (req, res) => {
+  try {
+    const {email, password} = re.body;
+    
+  } catch (error) {
+    
+  }
+})
 start();
